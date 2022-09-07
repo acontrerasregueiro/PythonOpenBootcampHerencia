9a9dfdfdc9358e8978fdcfb59f7fdc10dad5d638
Adrian <acontrerasregueiro@gmail.com>
2022-09-07 09:20:25 +0200

    Ejemplo con Constructor y con Metodos

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -7,11 +7,26 @@ class Alumno():
     nombre = ''
     nota = None
     
+    
+    #Ejemplo con constructor
     def __init__(self,nombre, nota):
         print('El nombre del alumno es : ', nombre)
         print('La nota del alumno es : ', nota)
+        
+    #Ejemplo con metodos    
+    def ponerNota(self,puntuacion):
+        nota = 6.6
+    
+    def ponerNombre(self,name):
+        nombre = name
            
 persona = Alumno("Javier Perez Perez", 7.8)
+
+persona2 = Alumno
+persona2.nombre = "Pedro Lopez Lopez"
+persona2.nota = 6.6
+
+print('La nota de ' ,persona2.nombre, 'Es : ',persona2.nota)
 
 
         
